d3eefe85cfe54fd9ee3239a471046e00f753826c
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-21 16:52:18 +0530

    [Shweta] Modified Employee Present or Not checked

--- EmployeeWageCompute_248/Program.cs
+++ EmployeeWageCompute_248/Program.cs
@@ -4,8 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("WelCome to Employee Wage Computaion Program");
-            Console.ReadLine();
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+            int isPresent = 1;
+            //UC1-EmployeeAttendance
+            Random random = new Random();
+            int EmpAttendance = random.Next(0, 2);
+            if (EmpAttendance == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
