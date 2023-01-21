ca6ed103f789459e4c10246bc822f0dbb87ce4f2
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-21 17:19:28 +0530

    [Shweta] Modified Daily Wage is Calculated

--- EmployeeWageCompute_248/Program.cs
+++ EmployeeWageCompute_248/Program.cs
@@ -6,17 +6,25 @@
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program");
             int isPresent = 1;
+            int EMPWagePerHr = 20;
+            int EmpHours = 8;
+            int DailyWage = 0;
             //UC1-EmployeeAttendance
             Random random = new Random();
             int EmpAttendance = random.Next(0, 2);
             if (EmpAttendance == isPresent)
             {
                 Console.WriteLine("Employee is Present");
+                EmpHours = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                EmpHours = 0;
             }
+            DailyWage = EmpHours * EMPWagePerHr;
+
+            Console.WriteLine("Daily Wage Of Employee is {0}", DailyWage);
         }
     }
 }
