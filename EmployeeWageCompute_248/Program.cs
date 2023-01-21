436f52ac71eedcf4f65ddb33310b92431c0bd97a
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-21 16:46:07 +0530

    [Shweta] Add Project of Employee Wage Computation

---
+++ EmployeeWageCompute_248/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageCompute_248
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to Employee Wage Computaion Program");
+            Console.ReadLine();
+        }
+    }
+}
+
