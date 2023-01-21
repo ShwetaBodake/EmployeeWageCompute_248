74c394e322277ca88f1ddd8b6eda50360e5ef640
Shweta Bodake <shwetabodake8108@gmail.com>
2023-01-21 19:13:44 +0530

    [Shweta] Modified Employee Wage Using Switch case

--- EmployeeWageCompute_248/Program.cs
+++ EmployeeWageCompute_248/Program.cs
@@ -5,29 +5,31 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation Program");
-            int isPresent = 1;
-            int isPartTime = 2;
-            int EMPWagePerHr = 20;
+           const int isPresent = 1;
+            const int isPartTime = 2;
+            const int EMPWagePerHr = 20;
             int EmpHours = 8;
             int DailyWage = 0;
             //UC1-EmployeeAttendance
             Random random = new Random();
             int EmpAttendance = random.Next(0, 3);
-            if (EmpAttendance == isPresent)
+            switch(EmpAttendance)
             {
-                Console.WriteLine("Employee is Present");
-                EmpHours = 8;
+                case isPresent:
+                    Console.WriteLine("Employee is Present");
+                    EmpHours = 8;
+                    break;
+                case isPartTime:
+                    Console.WriteLine("Employee is Part time Present");
+                    EmpHours = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    EmpHours = 0;
+                    break;
+
             }
-            else if(EmpAttendance== isPartTime)
-            {
-                Console.WriteLine("Employee is Part time Present");
-                EmpHours = 4;
-            }
-            else 
-            {
-                Console.WriteLine("Employee is Absent");
-                EmpHours = 0;
-            }
+         
             DailyWage = EmpHours * EMPWagePerHr;
 
             Console.WriteLine("Daily Wage Of Employee is {0}", DailyWage);
